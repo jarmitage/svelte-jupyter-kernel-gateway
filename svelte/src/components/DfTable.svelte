79b7a86ad1036641cacad84bb9dcfa22339643d8
Jack Armitage <jack.armitage@me.com>
2020-01-07 13:17:48 +0000

    Move table to component

---
+++ svelte/src/components/DfTable.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let caption
+	export let df
+</script>
+
+<style>
+	table {
+		table-layout: fixed;
+		width: 50%;
+		border-collapse: collapse;
+		border: 1px solid black;
+		text-align: left;
+	}
+	th, tr, td {
+		border: 1px solid black;
+	}
+</style>
+
+<table>
+	<caption>{caption}</caption>
+	<thead>
+		<tr>
+			{#each df.listColumns() as column}
+				<th>{column}</th>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each df.listColumns() as column, c}
+			<tr>
+				{#each df.getRow(c).toArray() as row, r}
+					<td>{row}</td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
--- svelte/src/containers/JupyterKernelGateway.svelte
+++ svelte/src/containers/JupyterKernelGateway.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { onMount } from 'svelte'
 	import { JupyterStore } from '../stores/jupyterStore.js'
+	import DfTable from '../components/DfTable.svelte'
 
 	import DataFrame from "dataframe-js";
 
@@ -8,7 +9,7 @@
 	$: received = false
 
 	onMount(async () => {
-		JupyterStore.setup('8889')
+		JupyterStore.setup()
 		await getDataFrame()
 	})
 
@@ -21,39 +22,8 @@
 
 </script>
 
-<style>
-	table {
-		table-layout: fixed;
-		width: 50%;
-		border-collapse: collapse;
-		border: 1px solid black;
-		text-align: left;
-	}
-	th, tr, td {
-		border: 1px solid black;
-	}
-</style>
-
 <p>Port: {$JupyterStore.port}</p>
 
 {#if received}
-	<table>
-		<caption>DataFrame</caption>
-		<thead>
-			<tr>
-				{#each df.listColumns() as column}
-					<th>{column}</th>
-				{/each}
-			</tr>
-		</thead>
-		<tbody>
-			{#each df.listColumns() as column, c}
-				<tr>
-					{#each df.getRow(c).toArray() as row, r}
-						<td>{row}</td>
-					{/each}
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+	<DfTable caption='DataFrame' df={df}/>
 {/if}
